0bb1ed2944508343e02c96469e01e26393a1e0d8
RichardHHill <richard_hill@brown.edu>
2019-07-04 18:13:14 -0400

    Removal now removes course from semester

--- shiny-app/server/03_server_courses/03.3_server_courses_remove.R
+++ shiny-app/server/03_server_courses/03.3_server_courses_remove.R
@@ -2,7 +2,9 @@
 
 observeEvent(input$semester_1_remove, {
   row <- as.numeric(input$semester_1_remove)
-  semester1_out <- semester1_out()[-row,]
   
+  semesters$semester1 <- semesters$semester1[-row]
+  
+  semester1_out <- semester1_out()[-row,]
   semester1_out(semester1_out)
 })
