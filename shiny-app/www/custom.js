39a8ba8d45597bee32a6f1cedf599794e96b2019
RichardHHill <richard_hill@brown.edu>
2019-07-09 22:46:25 -0400

    custom js ideas

--- shiny-app/www/custom.js
+++ shiny-app/www/custom.js
@@ -9,6 +9,15 @@ $(document).on("click", "#semester2_table .deselect_btn", function() {
 });
 
 
-$(document).on("click", "#set_inputs_1", function() {
+// use on charge or on mouse out instead of click
+// maybe .on("") 
+/*$(document).on("click", "#set_inputs_1", function() {
   console.log($('#major_1_courses_table > div > table').DataTable().cell(12,1).nodes().to$().find('input').val());
+})*/
+
+$(document).on("input", "#course_name_12", function(event) {
+  console.log("ran")
+  //event.target.value
+  //console.log(event); maybe find by class and then index by value
+  //console.log($('#major_1_courses_table > div > table').DataTable().cell(12,1).nodes().to$().find('input').val());
 })
