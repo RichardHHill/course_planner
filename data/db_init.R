c7623d0052b8ed5231fad6e096b3757cc94213e4
RichardHHill <richard_hill@brown.edu>
2019-07-17 17:35:58 -0400

    db init

---
+++ data/db_init.R
@@ -0,0 +1,43 @@
+library(dplyr)
+library(RPostgres)
+
+Sys.setenv("R_CONFIG_ACTIVE" = "default")
+app_config <- config::get(file = "shiny-app/config.yml")
+conn <- tychobratools::db_connect(app_config$db)
+
+ids_table_query <- "CREATE TABLE input_ids (
+  id                SERIAL PRIMARY KEY,
+  passkey           TEXT       
+);"
+
+DBI::dbExecute(conn, ids_table_query)
+
+semester_courses <- tibble(
+  id = numeric(0),
+  semester = character(0),
+  course = character(0)
+)
+
+DBI::dbWriteTable(
+  conn,
+  "semester_courses",
+  semester_courses,
+  row.names = FALSE
+)
+
+majors <- tibble(
+  id = numeric(0),
+  major_number = character(0),
+  major_name = character(0),
+  tag = character(0),
+  course = character(0)
+)
+
+DBI::dbWriteTable(
+  conn,
+  "majors",
+  majors,
+  row.names = FALSE
+)
+
+DBI::dbDisconnect(conn)
