1ecb1cd8351470a718a61891821b00b05e7dae52
RichardHHill <richard_hill@brown.edu>
2019-07-17 16:19:32 -0400

    Button and modal to save

--- shiny-app/server/03_s_courses/03.3_s_save_data.R
+++ shiny-app/server/03_s_courses/03.3_s_save_data.R
@@ -33,3 +33,69 @@ major_inputs_saved <- reactive({
   out
 })
 
+
+observeEvent(input$save_all_inputs, {
+  showModal(
+    modalDialog(
+      title = "Save Courses and Majors",
+      size = "s",
+      footer = list(
+        actionButton(
+          "confirm_save_all",
+          "Save All",
+          style = "background-color: #46c410; color: #fff",
+          icon = icon("plus")
+        ),
+        modalButton("Cancel")
+      )
+    )
+  )
+})
+
+
+observeEvent(input$confirm_save_all, {
+  removeModal()
+  courses_df <- courses_df()
+  major_inputs_saved <- major_inputs_saved()
+  dat <- courses_df
+  
+  progress <- Progress$new(session, min = 0, max = 8)
+  tryCatch({
+    DBI::dbWithTransaction(conn, {
+      progress$inc(amount = 1, message = "Saving Inputs", detail = "initializing...")
+      
+      
+    })
+    
+    session$sendCustomMessage(
+      "show_toast",
+      message = list(
+        type = "success",
+        title = "Inputs Successfully Saved!",
+        message = NULL
+      )
+    )
+    
+    loaded_input_set(hold_loaded_input_set)
+    
+    progress$close()
+  }, error = function(error) {
+    
+    session$sendCustomMessage(
+      "show_toast",
+      message = list(
+        type = "error",
+        title = "Error Saving Inputs",
+        message = NULL
+      )
+    )
+    progress$close()
+    print(error)
+  })
+  
+})
+
+
+
+
+
