6c4d07e22cbe3398bf431ac0c40c1cf7c8dd5b70
RichardHHill <richard_hill@brown.edu>
2019-07-20 11:44:49 -0400

    Add feature in individual semesters

--- shiny-app/modules/semester_module.R
+++ shiny-app/modules/semester_module.R
@@ -7,7 +7,21 @@ semester_module_ui <- function(id) {
     width = 3,
     box(
       width = 12,
-      title = paste0("Semester ", id),
+      fluidRow(
+        column(
+          6,
+          h4(paste0("Semester ", id))
+        ),
+        column(
+          6,
+          align = "right",
+          actionButton(
+            ns("add_course"),
+            " ",
+            icon = icon("plus")
+          )
+        )
+      ),
       DTOutput(ns("semester_table"))
     )
   )
@@ -16,6 +30,114 @@ semester_module_ui <- function(id) {
 semester_module <- function(input, output, session, id, delete_mode, semesters, semester_remove) {
   ns <- session$ns
   runjs(paste0("myModuleJS('", ns(""), "');"))
+  
+  observeEvent(input$add_course, {
+    showModal(
+      modalDialog(
+        title = "Add Course",
+        size = "m",
+        footer = list(
+          actionButton(ns("submit_course"), "Add Course"),
+          modalButton("Cancel")
+        ),
+        fluidRow(
+          column(
+            12,
+            align = "center",
+            radioButtons(
+              ns("pick_or_custom"),
+              " ",
+              choices = c("Select Course", "Custom"),
+              inline = TRUE
+            )
+          )
+        ),
+        conditionalPanel(
+          "input.pick_or_custom == 'Select Course'",
+          ns = ns,
+          fluidRow(
+            column(
+              6,
+              pickerInput(
+                ns("department_to_add"),
+                "Department",
+                choices = names(department_list)
+              )
+            ),
+            column(
+              6,
+              pickerInput(
+                ns("course_to_add"),
+                "Course",
+                choices = paste(
+                  department_list[[1]][[1]],
+                  department_list[[1]][[2]]
+                )
+              )
+            )
+          )
+        ),
+        conditionalPanel(
+          "input.pick_or_custom == 'Custom'",
+          ns = ns,
+          fluidRow(
+            column(
+              6,
+              textInput(
+                ns("code_to_add"),
+                "Course Code",
+                placeholder = "ECON 0110"
+              )
+            ),
+            column(
+              6,
+              textInput(
+                ns("name_to_add"),
+                "Course Name",
+                placeholder = "Principles of Econ"
+              )
+            )
+          )
+        )
+      )
+    )
+    
+    observeEvent(input$department_to_add, {
+      department <- input$department_to_add
+      updatePickerInput(
+        session,
+        "course_to_add", 
+        "Course",
+        choices = paste(
+          department_list[[department]][[1]],
+          department_list[[department]][[2]]
+        )
+      )
+    })
+  })
+  
+  observeEvent(input$submit_course, {
+    removeModal()
+    
+    type <- input$pick_or_custom
+    semester <- paste0("semester", id)
+    
+    if (type == "Custom") {
+      code <- input$code_to_add
+      
+      if (nchar(code) > 10) {
+        code <- substr(code,1,10)
+      } else if (nchar(code) < 10) {
+        code <- paste0(code, strrep(" ", 10 - nchar(code)))
+      }
+      
+      course <- paste(code, input$name_to_add)
+    } else {
+      course <- input$course_to_add
+    }
+    
+    semesters[[semester]] <- c(semesters[[semester]], course)
+  })
   
   semester_out <- reactiveVal()
   
