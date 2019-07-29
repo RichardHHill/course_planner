d89954f5fbb32378126b43264c84ae582e578936
RichardHHill <richard_hill@brown.edu>
2019-07-29 00:03:17 -0400

    Condensed data prep, added majors

--- data/data-prep.R
+++ data/data-prep.R
@@ -7,88 +7,30 @@ file_path <- "data/provided/Department Courses.xlsx"
 # Departments
 ###
 
-math_courses <- read.xlsx(
-  file_path,
-  sheet = 1,
-  rows = 2:34,
-  cols = 1:2
+department_excel_tribble <- tribble(
+  ~name, ~rows,
+  "MATH", 2:34,
+  "ECON", 2:41,
+  "APMA", 2:23,
+  "PHYS", 2:28,
+  "CSCI", 2:47,
+  "FREN", 2:28,
+  "HISP", 2:34,
+  "CLPS", 2:54,
+  "POLS", 2:37,
+  "PLCY", 2:17
 )
 
-econ_courses <- read.xlsx(
-  file_path,
-  sheet = 1,
-  rows = 2:41,
-  cols = 4:5
-)
+department_list <- lapply(1:nrow(department_excel_tribble), function(row) {
+  read.xlsx(
+    file_path,
+    sheet = 1,
+    rows = department_excel_tribble[[row, 2]],
+    cols = (3 * row - 2):(3 * row - 1)
+  )
+})
 
-apma_courses <- read.xlsx(
-  file_path,
-  sheet = 1,
-  rows = 2:23,
-  cols = 7:8
-)
-
-phys_courses <- read.xlsx(
-  file_path,
-  sheet = 1,
-  rows = 2:28,
-  cols = 10:11
-)
-
-csci_courses <- read.xlsx(
-  file_path,
-  sheet = 1,
-  rows = 2:47,
-  cols = 13:14
-)
-
-fren_courses <- read.xlsx(
-  file_path,
-  sheet = 1,
-  rows = 2:28,
-  cols = 16:17
-)
-
-hisp_courses <- read.xlsx(
-  file_path,
-  sheet = 1,
-  rows = 2:34,
-  cols = 19:20
-)
-
-clps_courses <- read.xlsx(
-  file_path,
-  sheet = 1,
-  rows = 2:54,
-  cols = 22:23
-)
-
-pols_courses <- read.xlsx(
-  file_path,
-  sheet = 1,
-  rows = 2:37,
-  cols = 25:26
-)
-
-plcy_courses <- read.xlsx(
-  file_path,
-  sheet = 1,
-  rows = 2:17,
-  cols = 28:29
-)
-
-department_list <- list(
-  "MATH" = math_courses,
-  "ECON" = econ_courses,
-  "APMA" = apma_courses,
-  "PHYS" = phys_courses,
-  "CSCI" = csci_courses,
-  "FREN" = fren_courses,
-  "HISP" = hisp_courses,
-  "CLPS" = clps_courses,
-  "POLS" = pols_courses,
-  "PLCY" = plcy_courses
-)
+names(department_list) <- department_excel_tribble$name
 
 for (i in seq_along(department_list)) {
   course_codes <- department_list[[i]]$course_code
@@ -108,80 +50,39 @@ saveRDS(department_list, "shiny-app/data/department_list.RDS")
 # Majors
 ###
 
-math_ab <- read.xlsx(
-  file_path,
-  sheet = 2,
-  rows = 2:21,
-  cols = 1:4,
-  colNames = FALSE
+major_excel_tribble <- tribble(
+  ~tag, ~sheet, ~rows, ~cols,
+  "apma_ab", "A", 2:19, 1:7,
+  "apma_scb", "A", 2:19, 9:18,
+  "apma_bio_scb", "A", 2:8, 21:34,
+  "apma_cs_scb", "A", 2:11, 37:49,
+  "apma_econ_adv_ab", "A", 22:54, 1:12,
+  "apma_econ_adv_scb", "A", 22:54, 29:40,
+  "apma_econ_mf_ab", "A", 22:54, 15:26,
+  "apma_econ_mf_ab", "A", 22:54, 43:54,
+  "bds_ab", "B", 1:25, 1:9,
+  "econ_ab", "E", 2:32, 1:7,
+  "math_ab", "M", 2:21, 1:4,
+  "math_scb", "M", 2:21, 9:15,
+  "math_cs_scb", "M", 2:35, 22:30,
+  "math_econ_ab", "M", 26:56, 1:12,
+  "physics_ab", "P", 27:32, 12:19,
+  "physics_scb", "P", 27:33, 22:34,
+  "psych_ab", "P", 2:22, 1:9,
+  "psych_scb", "P", 2:22, 12:22,
+  "public_ab", "P", 26:33, 1:7
 )
 
-econ_ab <- read.xlsx(
-  file_path,
-  sheet = 2,
-  rows = 26:56,
-  cols = 1:7,
-  colNames = FALSE
-)
-
-apma_ab <- read.xlsx(
-  file_path,
-  sheet = 2,
-  rows = 2:19,
-  cols = 10:16,
-  colNames = FALSE
-)
-
-mathcs_scb <- read.xlsx(
-  file_path,
-  sheet = 2,
-  rows = 2:35,
-  cols = 22:30,
-  colNames = FALSE
-)
-
-bds_ab <- read.xlsx(
-  file_path,
-  sheet = 2,
-  rows = 26:49,
-  cols = 12:20,
-  colNames = FALSE
-)
-
-psych_ab <- read.xlsx(
-  file_path,
-  sheet = 3,
-  rows = 2:22,
-  cols = 1:9,
-  colNames = FALSE
-)
-
-psych_scb <- read.xlsx(
-  file_path,
-  sheet = 3,
-  rows = 2:22,
-  cols = 12:22,
-  colNames = FALSE
-)
-
-public_ab <- read.xlsx(
-  file_path,
-  sheet = 3,
-  rows = 26:33,
-  cols = 1:7,
-  colNames = FALSE
-)
-
-majors_list <- list(
-  math_ab = math_ab,
-  econ_ab = econ_ab,
-  apma_ab = apma_ab,
-  mathcs_scb = mathcs_scb,
-  bds_ab = bds_ab,
-  psych_ab = psych_ab,
-  psych_scb = psych_scb,
-  public_ab = public_ab
-)
+majors_list <- lapply(1:nrow(major_excel_tribble), function(row) {
+  read.xlsx(
+    file_path,
+    sheet = major_excel_tribble[[row, 2]],
+    rows = major_excel_tribble[[row, 3]],
+    cols = major_excel_tribble[[row, 4]],
+    colNames = FALSE
+  )
+})
+names(majors_list) <- major_excel_tribble$tag
 
 for (i in seq_along(majors_list)) {
   courses <- majors_list[[i]][c(-1,-2,-3), ]
@@ -201,27 +102,8 @@ for (i in seq_along(majors_list)) {
 saveRDS(majors_list, "shiny-app/data/majors_list.RDS")
 
 majors_table <- tibble(
-  major = c(
-    "math_ab",
-    "econ_ab",
-    "apma_ab",
-    "mathcs_scb",
-    "bds_ab",
-    "psych_ab",
-    "psych_scb",
-    "public_ab"
-  ),
-  display = c(
-    "Math AB",
-    "Econ AB",
-    "Apma AB",
-    "Math-CS ScB",
-    "Behavioral Decision Sciences AB",
-    "Psychology AB",
-    "Psychology ScB",
-    "Public Policy AB"
-  )
+  major = names(majors_list),
+  display = lapply(names(majors_list), function(x) majors_list[[x]][1,2])
 )
 
 saveRDS(majors_table, "shiny-app/data/majors.RDS")
-
--- shiny-app/modules/major_table_module.R
+++ shiny-app/modules/major_table_module.R
@@ -46,7 +46,7 @@ major_table_module <- function(input, output, server, major_course_vector, name,
       }
     })
     
-    need_approval <- c("FlexCourse", "Capstone  ", "Approved  ")
+    need_approval <- c("FlexCourse", "Capstone  ", "Approved  ", "Equivalent")
     
     course_names[selected_courses %in% need_approval] <- "To be approved by a Concentration Advisor"
     in_schedule[selected_courses %in% need_approval] <- ""
