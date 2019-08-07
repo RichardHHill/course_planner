424dd2d9651a189677dfb9ba09115da15bf47aee
RichardHHill <richard_hill@brown.edu>
2019-08-06 23:43:16 -0400

    Datatables now editable

--- shiny-app/modules/major_table_module.R
+++ shiny-app/modules/major_table_module.R
@@ -31,11 +31,30 @@ major_table_module <- function(input, output, server, major_course_vector, name,
     }
   })
   
-  major_courses_table_prep <- reactive({
+  major_courses_table_prep <- reactiveVal()
+  
+  observe({
     req(major_course_vector())
-    selected_courses <- major_course_vector()
     
+    selected_courses <- major_course_vector()
     course_names <- unlist(lapply(selected_courses, helpers$course_code_to_name))
+    
+    need_approval <- c("FlexCourse", "Capstone  ", "Approved  ", "Equivalent")
+
+    course_names[selected_courses %in% need_approval] <- "To be approved by a Concentration Advisor"
+    selected_courses[selected_courses %in% need_approval] <- "Input Code"
+    
+    
+    out <- tibble(
+      "Course Code" = selected_courses,
+      "Course Name" = course_names
+    )
+    
+    major_courses_table_prep(out)
+  })
+  
+  courses_satisfied_column <- reactive({
+    selected_courses <- major_courses_table_prep()[["Course Code"]]
     
     in_schedule <- selected_courses %in% schedule_list()
     in_schedule <- lapply(in_schedule, function(x) {
@@ -45,21 +64,16 @@ major_table_module <- function(input, output, server, major_course_vector, name,
         as.character(icon("circle"))
       }
     })
-    
-    need_approval <- c("FlexCourse", "Capstone  ", "Approved  ", "Equivalent")
-    
-    course_names[selected_courses %in% need_approval] <- "To be approved by a Concentration Advisor"
-    in_schedule[selected_courses %in% need_approval] <- ""
 
-    tibble(
-      "Course Code" = selected_courses,
-      "Course Name" = course_names,
-      "Satisfied" = in_schedule
-    )
+    tibble("Satisfied" = unlist(in_schedule))
   })
   
   output$major_courses_table <- renderDT({
-    table <- major_courses_table_prep()
+    table <- bind_cols(
+      major_courses_table_prep(),
+      courses_satisfied_column()
+    )
+    
     datatable(
       table,
       rownames = FALSE,
@@ -67,8 +81,22 @@ major_table_module <- function(input, output, server, major_course_vector, name,
       options = list(
         dom = "t",
         pageLength = 25
-      )
+      ),
+      editable = TRUE,
+      selection = "none"
     )
+  })
+  
+  major_courses_proxy <- dataTableProxy("major_courses_table")
+
+  
+  observeEvent(input$major_courses_table_cell_edit, {
+    hold <- input$major_courses_table_cell_edit
+    out <- major_courses_table_prep()
+    
+    out[hold$row, hold$col + 1] <- hold$value
+    
+    major_courses_table_prep(out)
   })
   
 }
