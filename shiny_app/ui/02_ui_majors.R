eeaccf396fcb25dfaa6df8b6afb356b987827bb8
RichardHHill <richard_hill@brown.edu>
2019-08-10 14:45:31 -0400

    Majors ui

--- shiny_app/ui/02_ui_majors.R
+++ shiny_app/ui/02_ui_majors.R
@@ -13,46 +13,62 @@ tabItem(
   fluidRow(
     column(
       4,
-      box(
-        width = 12,
-        title = "Select Major",
-        id = "select_major_box",
-        collapsible = TRUE,
-        pickerInput(
-          "pick_major",
-          "",
-          choices = setNames(majors_table$major, majors_table$display)
-        ),
-        br(),
-        actionButton(
-          "get_requirements",
-          "Get Requirements"
-        )
-      ),
-      div(
-        id = "chosen_major_courses_box",
+      fluidRow(
         box(
           width = 12,
-          title = "Selected Courses",
-          div(
-            id = "major_chosen",
-            column(
-              12,
-              align = "center",
-              h2("Double click a cell to edit"),
-              br(),
-              textInput("submit_major_name", "Name", width = "33%"),
-              actionButton(
-                "submit_major",
-                "Submit Major",
-                style = "color: #fff; background-color: #07b710; border-color: #07b710;"
-              )
-            )
+          title = "Select Major",
+          id = "select_major_box",
+          collapsible = TRUE,
+          pickerInput(
+            "pick_major",
+            "",
+            choices = setNames(majors_table$major, majors_table$display)
           ),
           br(),
-          DTOutput("major_courses_table")
+          actionButton(
+            "get_requirements",
+            "Get Requirements"
+          )
+        ),
+        div(
+          id = "chosen_major_courses_box",
+          box(
+            width = 12,
+            title = "Selected Courses",
+            div(
+              id = "major_chosen",
+              column(
+                12,
+                align = "center",
+                h2("Double click a cell to edit"),
+                br(),
+                textInput("submit_major_name", "Name", width = "33%"),
+                actionButton(
+                  "submit_major",
+                  "Submit Major",
+                  style = "color: #fff; background-color: #07b710; border-color: #07b710;"
+                )
+              )
+            ),
+            br(),
+            DTOutput("major_courses_table")
+          )
+        ) %>% hidden()
+      )
+    ),
+    box(
+      width = 4,
+      title = "Custom Major",
+      column(
+        12,
+        align = "center",
+        textInput("submit_custom_major_name", "Name", width = "33%"),
+        actionButton(
+          "submit_custom_major",
+          "Submit Major",
+          style = "color: #fff; background-color: #07b710; border-color: #07b710;"
         )
-      ) %>% hidden()
+      )
     ),
     box(
       width = 4,
@@ -60,9 +76,4 @@ tabItem(
       DTOutput("built_majors_table")
     )
   )
-  # fluidRow(
-  #   input_major_module_ui(id = "1"),
-  #   input_major_module_ui(id = "2"),
-  #   input_major_module_ui(id = "3")
-  # )
 )
