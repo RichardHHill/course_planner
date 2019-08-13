a04f164bd51ebd74c2b7367d2bb429d40f55181b
RichardHHill <richard_hill@brown.edu>
2019-08-13 18:38:19 -0400

    Removed dashboard and reorganized app

--- shiny_app/server.R
+++ shiny_app/server.R
@@ -1,15 +1,13 @@
 server <- function(input, output, session) {
-  source("server/01_s_dashboard.R", local = TRUE)$value
-  
+  source("server/01_s_semesters/01.1_s_semesters.R", local = TRUE)$value
+  source("server/01_s_semesters/01.2_s_major_tables.R", local = TRUE)$value
+
   source("server/02_s_majors/02.1_s_build_major.R", local = TRUE)$value
   source("server/02_s_majors/02.2_s_custom_major.R", local = TRUE)$value
   source("server/02_s_majors/02.3_s_store_majors.R", local = TRUE)$value
   
-  source("server/03_s_semesters/03.1_s_semesters.R", local = TRUE)$value
-  source("server/03_s_semesters/03.2_s_major_tables.R", local = TRUE)$value
-  
-  source("server/04_s_save/04.1_s_save_data.R", local = TRUE)$value
-  source("server/04_s_save/04.2_s_saved_table.R", local = TRUE)$value
-  source("server/04_s_save/04.3_s_delete.R", local = TRUE)$value
-  source("server/04_s_save/04.4_s_reload.R", local = TRUE)$value
+  source("server/03_s_save/03.1_s_save_data.R", local = TRUE)$value
+  source("server/03_s_save/03.2_s_saved_table.R", local = TRUE)$value
+  source("server/03_s_save/03.3_s_delete.R", local = TRUE)$value
+  source("server/03_s_save/03.4_s_reload.R", local = TRUE)$value
 }
--- shiny_app/ui.R
+++ shiny_app/ui.R
@@ -19,19 +19,14 @@ sidebar <- dashboardSidebar(
   sidebarMenu(
     id = "sidebar",
     menuItem(
-      text = "Dashboard",
-      tabName = "dashboard",
-      icon = icon("home")
+      text = "Select Courses",
+      tabName = "select_courses",
+      icon = icon("clipboard-list")
     ),
     menuItem(
       text = "Select Majors",
       tabName = "select_majors",
       icon = icon("graduation-cap")
-    ),
-    menuItem(
-      text = "Select Courses",
-      tabName = "select_courses",
-      icon = icon("clipboard-list")
     ),
     menuItem(
       text = "Saved Inputs",
@@ -51,10 +46,9 @@ body <- dashboardBody(
   useShinyjs(),
   extendShinyjs(script = "www/shinyjs.js"),
   tabItems(
-    source("ui/01_ui_dashboard.R", local = TRUE)$value,
+    source("ui/01_ui_courses.R", local = TRUE)$value,
     source("ui/02_ui_majors.R", local = TRUE)$value,
-    source("ui/03_ui_courses.R", local = TRUE)$value,
-    source("ui/04_ui_saved_inputs.R", local = TRUE)$value
+    source("ui/03_ui_saved_inputs.R", local = TRUE)$value
   )
 )
 
