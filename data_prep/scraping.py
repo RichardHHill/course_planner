33f66d76cc7e46e0caf513e63cd8cbc33ee35f96
RichardHHill <richard_hill@brown.edu>
2020-07-23 11:04:06 -0400

    Update gitignore, remove unneeded imports from scraping

--- data_prep/scraping.py
+++ data_prep/scraping.py
@@ -2,9 +2,6 @@
 import sqlite3
 from bs4 import BeautifulSoup
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import Select
-from selenium.webdriver.support.ui import WebDriverWait
 import chromedriver_binary
 import time
 
